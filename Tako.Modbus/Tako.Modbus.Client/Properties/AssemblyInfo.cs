b199003ce02497e49657fc7499a54bd894fa2a14
余小章 <SND\yc421206_cp>
2015-02-16 02:08:28 +0000

    變更版號

--- Tako.Modbus/Tako.Modbus.Client/Properties/AssemblyInfo.cs
+++ Tako.Modbus/Tako.Modbus.Client/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
 //[assembly: AssemblyKeyFile("yao-chang.yu@outlook.com.snk")]
 //[assembly: AssemblyKeyName("Tako.Modbus.Client, Version=1.0.0.1, Culture=neutral, PublicKeyToken=275e7bf739ea2aa4")]
 //[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Tako.Modbus.Client.UnitTest")]
